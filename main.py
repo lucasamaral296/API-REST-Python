2b6a06ffe737fed58d7a17353267144e5b0e22b0
Lucas Amaral Velasque <81877529+lucasamaral296@users.noreply.github.com>
2023-07-26 12:10:57 -0300

    comentarios sobre rest

---
+++ main.py
@@ -0,0 +1,12 @@
+'''
+API - Application Programming Interface
+
+REST:
+
+GET - Para pegar uma informação
+POST - Para criar uma informação
+PUT(PATCH) - Para atualizar uma informação
+DELETE - Para deletar uma informação
+
+'''
+
