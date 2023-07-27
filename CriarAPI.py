e1967d4f0b534ca9fc62aacfc5c731c20d7cc0c6
Lucas Amaral Velasque <81877529+lucasamaral296@users.noreply.github.com>
2023-07-27 11:57:08 -0300

    Entendendo API Flask

--- CriarAPI.py
+++ CriarAPI.py
@@ -1,9 +1,17 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__)  # Cria o Site    
 
-@app.route("/")   # decorator
+@app.route("/")   # decorator  -  Diz em qual link a função vai rodar
 def hello_world():  # função
-    return "<p>Esta no Ar!</p>"
+    return {"Lucas": "Dev em Crescimento"}
 
-app.run(debug=True)
+@app.route("/teste")   # decorator  -  Diz em qual link a função vai rodar
+def teste():  # função
+    return {"Teste": "Em Andamento"}
+
+@app.route("/outroteste")   # decorator  -  Diz em qual link a função vai rodar
+def outroteste():  # função
+    return {"Outro Teste": "Em Andamento Tambem", "Nome": "Lucas", "Dev": "Python"}
+
+app.run(debug=True)  # coloca o site no ar
